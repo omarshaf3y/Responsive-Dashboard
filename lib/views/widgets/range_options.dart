1f4325b9e7fa46caa2b3b59b09f612297231a7ef
omarshaf3y <omarshaf3y99@gmail.com>
2024-09-29 18:24:24 +0300

    All expenses header

---
+++ lib/views/widgets/range_options.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class RangeOptions extends StatelessWidget {
+  const RangeOptions({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          width: 1,
+          color: const Color(0xffF1F1F1),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'Monthly',
+            style: AppStyles.styleMedium16,
+          ),
+          const SizedBox(width: 18),
+          Transform.rotate(
+            angle: -1.5708,
+            child: const Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Color(0xff064060),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

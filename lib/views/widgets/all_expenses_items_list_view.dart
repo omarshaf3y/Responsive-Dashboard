9348be4780e88e908e14661c7327ddb510886603
omarshaf3y <omarshaf3y99@gmail.com>
2024-10-06 17:46:03 +0300

    All expenses section

--- lib/views/widgets/all_expenses_items_list_view.dart
+++ lib/views/widgets/all_expenses_items_list_view.dart
@@ -3,47 +3,89 @@ import 'package:responsive_dashboard/models/all_expenses_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
 import 'package:responsive_dashboard/views/widgets/all_expneses_item.dart';
 
-class AllExpensesItemsListView extends StatelessWidget {
+class AllExpensesItemsListView extends StatefulWidget {
   const AllExpensesItemsListView({super.key});
 
-  static const items = [
-    AllExpensesItemModel(
-      image: Assets.imagesBalance,
-      title: 'Balance',
-      date: 'Sept 2024',
-      price: r'$20.129',
-    ),
-    AllExpensesItemModel(
-      image: Assets.imagesIncome,
-      title: 'Income',
-      date: 'Sept 2024',
-      price: r'$20.129',
-    ),
-    AllExpensesItemModel(
-      image: Assets.imagesExpenses,
-      title: 'Expenses',
-      date: 'Sept 2024',
-      price: r'$20.129',
-    ),
+  @override
+  State<AllExpensesItemsListView> createState() =>
+      _AllExpensesItemsListViewState();
+}
+
+class _AllExpensesItemsListViewState extends State<AllExpensesItemsListView>
+    with SingleTickerProviderStateMixin {
+  final items = [
+    const AllExpensesItemModel(
+        image: Assets.imagesBalance,
+        title: 'Balance',
+        date: 'April 2022',
+        price: r'$20,129'),
+    const AllExpensesItemModel(
+        image: Assets.imagesIncome,
+        title: 'Income',
+        date: 'April 2022',
+        price: r'$20,129'),
+    const AllExpensesItemModel(
+        image: Assets.imagesExpenses,
+        title: 'Expenses',
+        date: 'April 2022',
+        price: r'$20,129'),
   ];
 
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: items.asMap().entries.map((e) {
-        int index = e.key;
-        var item = e.value;
+      children: [
+        _buildAnimatedItem(0),
+        const SizedBox(width: 8),
+        _buildAnimatedItem(1),
+        const SizedBox(width: 8),
+        _buildAnimatedItem(2),
+      ],
+    );
+  }
 
-        return Expanded(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: index == 1 ? 12 : 0),
+  Widget _buildAnimatedItem(int index) {
+    final bool isSelected = selectedIndex == index;
+
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          updateIndex(index);
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          transform: Matrix4.identity()
+            ..scale(isSelected ? 1.1 : 1.0), // Scale animation
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: isSelected
+                ? [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    )
+                  ]
+                : [],
+          ),
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: isSelected ? 1.0 : 0.7, // Fade effect
             child: AllExpensesItem(
-              itemModel: item,
-              isSelected: false,
+              isSelected: isSelected,
+              itemModel: items[index],
             ),
           ),
-        );
-      }).toList(),
+        ),
+      ),
     );
+  }
+
+  void updateIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }

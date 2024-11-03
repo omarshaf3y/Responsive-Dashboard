c13b462b6caed2a05323ca9b55e330b8f3561b7d
omarshaf3y <omarshaf3y99@gmail.com>
2024-11-03 19:06:38 +0200

    Quick invoice part one

---
+++ lib/views/widgets/quick_invoice.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/views/widgets/custom_background_container.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const QuickInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgroundContainer(child: SizedBox());
+  }
+}
--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/all_expenses.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
+import 'package:responsive_dashboard/views/widgets/quick_invoice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -17,7 +18,8 @@ class DashboardDesktopLayout extends StatelessWidget {
             flex: 2,
             child: Column(
               children: [
-                Expanded(child: AllExpenses()),
+                AllExpenses(),
+                QuickInvoice(),
               ],
             )),
       ],

a36b38f3065064d148a6654a7f9114ba30a251e9
omarshaf3y <omarshaf3y99@gmail.com>
2024-12-21 13:48:41 +0200

    remove unused code

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/views/widgets/all_expenses.dart';
 import 'package:responsive_dashboard/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
-import 'package:responsive_dashboard/views/widgets/quick_invoice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
